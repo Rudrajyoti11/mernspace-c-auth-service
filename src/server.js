c6c2e7d79a50a445a846dffc5dadb192cf9ad791
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2026-08-08 11:21:46 +0530

    add typescript

--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
-console.log("Hi Rudra")
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function welcome(name) {
+    console.log('welcome ,' + name);
+}
+welcome('Rudra');
+//# sourceMappingURL=server.js.map
