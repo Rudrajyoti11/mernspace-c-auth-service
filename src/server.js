01e52a24082a378c3ed823dc1d2213623bb0f14f
Rudrajyoti11 <92710279+Rudrajyoti11@users.noreply.github.com>
2026-08-10 13:32:28 +0530

    add config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,2 @@
 'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-function welcome(name) {
-    console.log('welcome ,' + name);
-}
-welcome('Rudra');
-//# sourceMappingURL=server.js.map
+console.log(process.env.PORT);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,3 @@
-function welcome(name: string) {
-    return name;
-}
+import { Config } from './config/index.js';
 
-welcome('Rudra');
+console.log(Config.PORT);
